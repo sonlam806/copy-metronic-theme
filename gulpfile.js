f874e9b0e4bbc5ec0e51730c2abd7833cd07704e
SonLam <shindo@Shindos-iMac.local>
2020-09-01 13:48:04 +0700

    initial commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
-var gulp = require('gulp');
-var rename = require('gulp-rename');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const rename = require('gulp-rename');
+const postcss = require('gulp-postcss');
+
+sass.compiler = require('node-sass');
 
 const css = function () {
-  const postcss = require('gulp-postcss');
-
   return (
     gulp
       .src('src/tailwind/tailwind.css')
@@ -22,6 +24,18 @@ const css = function () {
   );
 };
 
+const scss = function () {
+  gulp
+    .src('src/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(rename('custom_style.css'))
+    .pipe(gulp.dest('src/styles'));
+};
+
 gulp.task('watch', function () {
   return gulp.watch('src/tailwind/*.css', css);
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./scss/**/*.scss', [scss, css]);
 });
